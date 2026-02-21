6566e759d5a7f7eea7de00257b48082c576bf27d
Taskflow <taskflow016@users.noreply.github.com>
2026-02-21 09:08:28 +0100

    email

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -3,14 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: false, // true only if using port 465
+  secure: false, // false for 587
+  requireTLS: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-// VERIFY CONNECTION 
 transporter.verify(function (error, success) {
   if (error) {
     console.log("SMTP CONNECTION ERROR:", error);
