a5462db134b1ac86334f5d08e68dc4360d627c51
Taskflow <taskflow016@users.noreply.github.com>
2026-03-06 23:39:43 +0100

    update

--- src/routes/invitation.routes.js
+++ src/routes/invitation.routes.js
@@ -14,6 +14,6 @@ const {
 //router.post("/", authMiddleware, inviteMember);
 
 // Accept invitation
-router.post("/accept", authMiddleware, acceptInvitation);
+router.post("/accept", acceptInvitation);
 
 module.exports = router;
